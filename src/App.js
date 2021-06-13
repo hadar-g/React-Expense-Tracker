b0a95e1b4978a65c5bd640e5548b35802c31544c
hadar-g <hadar.goldstein@gmail.com>
2021-06-13 16:05:16 +0300

    changed day, month, year display

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import ExpenseItem from'./components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    {title: 'Insurance', 
+    amount: 294.56, 
+    date: new Date(2021, 2, 28)
+  },
+  ]
   return (
     <div>
       <h1>Hi i'm a react project</h1>
-      <ExpenseItem />
+      <ExpenseItem 
+      title = {expenses[0].title}
+      amount = {expenses[0].amount}
+      date = {expenses[0].date}
+      ></ExpenseItem>
     </div>
   );
 }

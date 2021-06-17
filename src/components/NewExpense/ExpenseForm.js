073e64bfefa9a76cce3796a0b2e679efb9c5620e
hadar-g <hadar.goldstein@gmail.com>
2021-06-17 19:22:10 +0300

    added state and   event listeners to expense form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,23 +1,39 @@
 import './ExpenseForm.css'
+import React, {useState} from 'react'
 
 const ExpenseForm = () => {
+
+    const[enteredTitle, setEnteredTitle] = useState('')
+    const[enteredAmount, setEnteredAmount] = useState('')
+    const[enteredDate, setEnteredDate] = useState('')
+
+    const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value)
+    }
+    const amountChangeHandler  = (event) =>  {
+        setEnteredAmount(event.target.value)
+    }
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value)
+    }
+
 
     return(
         <form>
             <div className = 'new-expense__controls'>
                 <div className = 'new-expense__control'>
                     <label> Title </label>
-                    <input type ="text"/>
+                    <input type ="text" onChange = {titleChangeHandler}/>
                 </div>
 
                 <div className = 'new-expense__control'>
-                    <label> Amount </label>
-                    <input type ="text"/>
+                    <label> Amount  </label>
+                    <input type ="text"  onChange ={amountChangeHandler}/>
                 </div>
 
                 <div className = 'new-expense__control'>
                     <label> Date </label>
-                    <input type ='date' min="2019-01-01" max = "2022-12-31"/>
+                    <input type ='date' min="2019-01-01" max = "2022-12-31" onChange ={dateChangeHandler}/>
                 </div>
 
             </div>

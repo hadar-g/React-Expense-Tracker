66747095d2155d92b5565d4f6c100afde236e6cf
hadar-g <hadar.goldstein@gmail.com>
2021-06-23 16:38:22 +0300

    commit un-committed changes from last session

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import Expenses from'./components/Expenses/Expenses.js';
 import NewExpense from './components/NewExpense/NewExpense.js';
+import React, {useState} from 'react'
 
+const DUMMY_EXPENSES = [
+  {id: 'e1',
+  title: 'Insurance', 
+  amount: 294.56, 
+  date: new Date(2021, 2, 28)
+},
+{id: 'e2',
+title: 'car',
+amount: 54.90,
+date: new Date(2021,3,27)
+},
+]
 
 function App() {
-  const expenses = [
-    {title: 'Insurance', 
-    amount: 294.56, 
-    date: new Date(2021, 2, 28)
-  },
-  {title: 'car',
-  amount: 54.90,
-  date: new Date(2021,3,27)
-  },
-  ]
+
+  const[expenses, setExpenses] = useState(DUMMY_EXPENSES)
+  
 
   const addExpenseHandler = (expense) => {
-    console.log(expense)
+    //setExpenses((prevExpenses) =>{
+    //  return[expense, ...prevExpenses]
+    //})
+    setExpenses(prevExpenses => {
+      return[expense, ...prevExpenses]
+    })
   }
   return (
+
     <div>
       <NewExpense onAddExpense = {addExpenseHandler}/>
       <Expenses expenses = {expenses} />

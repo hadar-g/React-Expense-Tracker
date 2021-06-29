0b5e96ad31d9296e481ca78316218b631db34f37
hadar-g <hadar.goldstein@gmail.com>
2021-06-29 15:29:24 +0300

    added conditional contnet for if no expenses are found

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,20 +15,26 @@ function Expenses(props){
     const filteredExpenses = props.expenses.filter(expense =>{
         return expense.date.getFullYear().toString() === year
     })
+
+    let expensesContent = <p>No Expenses found.</p>;
+
+    if(filteredExpenses.length > 0){
+        expensesContent = filteredExpenses.map((expense) =>
+        ( <ExpenseItem 
+        key = {expense.id}
+        title = {expense.title}
+        amount = {expense.amount}
+        date = {expense.date}
+        />))
+    }
+
     return(
 
     <div>
 
         <Card className = 'expenses'>
-
         <ExpenseFilter selected = {year} onChangeYear = {filterChangeHandler}/>
-        
-        {filteredExpenses.map((expense) =>( <ExpenseItem 
-        title = {expense.title}
-        amount = {expense.amount}
-        date = {expense.date}
-        />))}
-        
+        {expensesContent}
         </Card>
 
     </div>

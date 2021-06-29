95ca608ceaf3fd84ea884b6ddef7a4ab5812d274
hadar-g <hadar.goldstein@gmail.com>
2021-06-29 16:19:35 +0300

    laid logic for Chart.js

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Chart = (props) => {
 
-
-
+    return <div className = "chart">
+    {props.dataPoints.map(dataPoint =>
+         <ChartBar 
+         key = {dataPoint.label}
+         value = {dataPoint.value} 
+         maxValue = {null}
+         label = {dataPoint.label}/>)}
+     </div>
 
 }
 
--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,6 +1,9 @@
 import  React from  'react'
+import './Chart.css'
+import ChartBar from './ChartBar.js'
 
 const ChartBar = props => {
+    
 
 }
 
